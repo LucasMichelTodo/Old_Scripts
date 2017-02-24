c56b61b0984fe9de6fabe9b6dbf7ab731c0de3bc
LucasMichelTodo <micheltod@gmail.com>
2017-02-24 12:06:47 +0100

    Calcular el coverage de les regions codificant (amb un while)

--- pysamer.py
+++ pysamer.py
@@ -8,7 +8,24 @@ import subprocess
 import pysam
 from tqdm import tqdm
 import csv
+import pandas
+import numpy
 
+# Get reference annotated genome
+
+ref_fasta = open("/home/lucas/ISGlobal/Gen_Referencies/PlasmoDB-30_Pfalciparum3D7_AnnotatedCDSs.fasta", "r+")
+coding_regions = {}
+for line in ref_fasta:
+	if line.startswith(">"):
+		gene = line.split("|")
+		gene = map(lambda x: x.strip(" >\n"), gene)
+		dict_string = map(lambda x: x.split("="), gene[1:])
+		gene_dict = {}
+		for element in dict_string:
+			gene_dict[element[0]] = element[1]
+		coding_regions[gene[0]] = gene_dict
+coding_df = pandas.DataFrame.from_dict(data=coding_regions, orient = "index")
+#print df
 
 def get_frag_len(samfile):
 	sam = pysam.AlignmentFile(samfile, "r")
@@ -57,11 +74,38 @@ def get_flags(samfile):
 			flags[read.flag] = 1
 	print flags
 
+def get_coding_coverage(bamfile):
+	sam = pysam.AlignmentFile(bamfile, "rb")
+	
+	regions = coding_df["location"]
+	references = []
+	starts = []
+	ends = []
+	for element in regions:
+		references.append(element.split(":")[0])
+		starts.append(element.split(":")[1].strip("([+-])").split("-")[0])
+		ends.append(element.split(":")[1].strip("([+-])").split("-")[1])
+
+	coverage = [] 
+	index = 0
+	while index <= len(references)-1:
+		cov = sam.count_coverage(reference=references[index], start=int(starts[index]), end=int(ends[index]))
+		total_cov = numpy.sum(cov, axis=0)
+		coverage.append(total_cov)
+		index += 1
+
+	coverage = numpy.concatenate(coverage, axis=0)
+	print coverage
+	print numpy.mean(coverage)
+
+
+
 filenames = sys.argv[1:]
 print filenames
 for element in tqdm(filenames):
+	get_coding_coverage(element)
 	#get_flags(element)
 	#get_unaligned_mate(element)
 	#get_unaligned(element)
-	get_frag_len(element)
+	#get_frag_len(element)
 	#get_MAPQ(element)
