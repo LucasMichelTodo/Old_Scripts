26660f449871b0318ac38936ebac02dffdf1a233
LucasMichelTodo <micheltod@gmail.com>
2017-02-28 09:31:22 +0100

    Abans de començar, guardar lo dahir

--- pysamer.py
+++ pysamer.py
@@ -25,7 +25,72 @@ for line in ref_fasta:
 			gene_dict[element[0]] = element[1]
 		coding_regions[gene[0]] = gene_dict
 coding_df = pandas.DataFrame.from_dict(data=coding_regions, orient = "index")
-#print df
+
+
+## Creating non-coding regions reference:
+
+regions = coding_df["location"]
+
+sizes = {}
+with open("/home/lucas/ISGlobal/Gen_Referencies/Pf3D7.sizes", "rb") as csvfile:
+	chrom_sizes = csv.reader(csvfile, delimiter='\t')
+	for row in chrom_sizes:
+		if len(row) == 2:
+			sizes[row[0]] = row[1]
+		else:
+			pass
+
+sizes["Pf_M76611"] = sizes.pop("M76611")
+sizes["Pf3D7_API_v3"] = sizes.pop("PFC10_API_IRAB")
+
+chrom = map(lambda x: x.split(":"), regions)
+chroms = {}
+for pair in chrom:
+	if pair[0] in chroms:
+		chroms[pair[0]].append(pair[1])
+	else:
+		chroms[pair[0]] = [pair[1]]
+
+for key in chroms:
+	chroms[key] = map(lambda x: x.strip("([+-])").split("-"), chroms[key])
+
+noncoding = {}
+for key in chroms:
+	noncoding[key] = []
+	for i in chroms[key]:
+#if i > noncoding[key][-1]:
+		noncoding[key].append(int(i[0])-1)
+		noncoding[key].append(int(i[1])+1)
+
+
+
+
+for key in noncoding:
+	noncoding[key].insert(0,1)
+	noncoding[key].append(sizes[key])
+
+## Remove overlapping gene
+#del noncoding["Pf3D7_09_v3"][73:75]
+#print range(len(noncoding["Pf3D7_09_v3"])+2)[2:]
+
+for key in noncoding:
+	for i in range(len(noncoding[key])):
+		if noncoding[key][i] < noncoding[key][i-1]:
+			print "Overlap in Chrom: {} position {}" .format(key, i)
+			print noncoding[key][i-2:i+2]
+			if i in range(len(noncoding[key])+3)[2:]:
+				if noncoding[key][i-2:i+2]:
+					noncoding[key][i-2:i+2] = [min(noncoding[key][i-2:i+2]),max(noncoding[key][i-2:i+2])]
+			else:
+				pass
+
+for key in noncoding:
+	for i in range(len(noncoding[key])):
+		if noncoding[key][i] < noncoding[key][i-1]:
+			print "Encara en queden..."
+
+
+### FUNCTIONS
 
 def get_frag_len(samfile):
 	sam = pysam.AlignmentFile(samfile, "r")
@@ -54,7 +119,6 @@ def get_unaligned(samfile):
 												samfile.replace(".sam", "_unmapped.sam"),
 												samfile.replace(".sam", "_mateunmapped.sam"))
 	subprocess.call(cmd, shell=True)
-
 	
 def get_unaligned_mate(samfile):
 	sam = pysam.AlignmentFile(samfile, "r")
@@ -88,79 +152,63 @@ def get_coding_coverage(bamfile):
 
 	coverage = []
 	for index in tqdm(range(len(references))):
+		#print "{} {} {}" .format(references[index], starts[index], ends[index])
 		cov = sam.count_coverage(reference=references[index], start=int(starts[index]), end=int(ends[index]))
 		total_cov = numpy.sum(cov, axis=0)
 		coverage.append(total_cov) ## Assuming coding regions do NOT OVERLAP
 
 	coverage = numpy.concatenate(coverage, axis=0)
-	print coverage
-	print numpy.mean(coverage)
+	print "Coding coverage: {}" .format(coverage)
+	print "Coding mean coverage: {}" .format(numpy.mean(coverage))
 
-# regions = coding_df["location"]
-# chromosomes = []
-# for element in regions:
-# 	chromosomes.append(element.split(":")[0])
-
-# chrom_series = pandas.Series(chromosomes)
-# print chrom_series.value_counts()
-# print numpy.unique(chromosomes)
-
-regions = coding_df["location"]
-#references = []
-#starts = []
-#ends = []
-chrom = map(lambda x: x.split(":"), regions)
-chroms = {}
-for pair in chrom:
-	if pair[0] in chroms:
-		chroms[pair[0]].append(pair[1])
-	else:
-		chroms[pair[0]] = [pair[1]]
-
-for key in chroms:
-	chroms[key] = map(lambda x: x.strip("([+-])").split("-"), chroms[key])
-
-noncoding = {}
-for key in chroms:
-	noncoding[key] = []
-	for i in chroms[key]:
-		noncoding[key].append(int(i[0])-1)
-		noncoding[key].append(int(i[1])+1)
-
-for key in noncoding:
-	noncoding[key].insert(0,1)
-	noncoding[key].append(999999999)
-
-print noncoding
+	nc_references = []
+	nc_starts = []
+	nc_ends = []
+	nc_coverage = []
+	i = 0
+	for key in noncoding:
+		for x in noncoding[key]:
+			if i == 0:
+				nc_references.append(key)
+				nc_starts.append(x)
+				i = 1
+			else:
+				nc_ends.append(x)
+				i = 0
 
 
-#print[noncoding]
+	for index in tqdm(range(len(nc_references))):
+		print "{} {} {}" .format(nc_references[index], nc_starts[index], nc_ends[index])
+		nc_cov = sam.count_coverage(reference=nc_references[index], start=int(nc_starts[index]), end=int(nc_ends[index]))
+		#print (nc_cov)
+		nc_total_cov = numpy.sum(nc_cov, axis=0)
+		#print nc_total_cov
+		nc_coverage.append(nc_total_cov)
+		#print (nc_coverage) 
 
-# df = pandas.DataFrame(chrom)
-# df.columns = ["chrom","idx"]
-# for element in df["chrom"]:
-# 	element
-# print df
-
-# chroms = {}
-# for element in regions:
-# 	indxs = []
-# 	chroms[element.split(":")[0]] = indxs.append(element.split(":")[1])
-
-# print chroms
-# chroms_df = pandas.DataFrame.from_dict(data=chroms, orient = "index")
-# print chroms_df
-# 	starts.append(element.split(":")[1].strip("([+-])").split("-")[0])
-# 	ends.append(element.split(":")[1].strip("([+-])").split("-")[1])
-
-# nc_idx = [1]
-# 	for element in regions:
-# 		nc_idx.append(element.split(":")[1].strip("([+-])").split("-")[0]-1)
-# 		nc_idx.append(element.split(":")[1].strip("([+-])").split("-")[0]+1)
-# 	nc_idx.append(#longitud del chrom)
+	nc_coverage = numpy.concatenate(nc_coverage, axis=0)
+	print "Non-Coding coverage: {}" .format(nc_coverage)
+	print "Non-Coding mean coverage: {}" .format(numpy.mean(nc_coverage))
 
 
+# nc_references = []
+# nc_starts = []
+# nc_ends = []
+# nc_coverage = []
+# i = 0
+# for key in noncoding:
+# 	for x in noncoding[key]:
+# 		if i == 0:
+# 			nc_references.append(key)
+# 			nc_starts.append(x)
+# 			i = 1
+# 		if i == 1:
+# 			nc_ends.append(x)
+# 			i = 0
 
+# print len(nc_references)
+# print len(nc_starts)
+# print len(nc_ends)
 
 filenames = sys.argv[1:]
 print filenames
