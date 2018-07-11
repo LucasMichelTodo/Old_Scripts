508ac4532f50de097d727cc34f7f63d3c9855b71
Lucas <micheltod@gmail.com>
2018-07-11 11:49:04 +0200

    Gran borrada d'arxius

--- annotate_variants.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-gff = {}
-with open("/home/lucas/ISGlobal/Gen_Referencies/PlasmoDB-31_Pfalciparum3D7_Sorted_filtered.gff", "r") as file1:
-	for line in file1:
-		gff[line.plit()[8].split(";")[1].replace("ID=", "")]={"start":line.plit()[3], "stop":line.plit()[4], "chrom":line.plit()[0], "strain":line.plit()[6], "anot":line.plit()[8]}
-
-print gff[1:3]
